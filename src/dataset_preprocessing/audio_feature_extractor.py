4651fc11be939a214668bbfe48039a4d7c9de2d3
caleo-hub <caleomenesessantos@gmail.com>
2023-04-23 15:28:34 -0300

    ae fixed

--- src/dataset_preprocessing/audio_feature_extractor.py
+++ src/dataset_preprocessing/audio_feature_extractor.py
@@ -1,8 +1,7 @@
 import h5py
 import librosa
 import numpy as np
-from scipy.interpolate import interp1d
-
+import scipy.signal as signal
 
 
 class AudioFeatureExtractor:
@@ -10,31 +9,22 @@ class AudioFeatureExtractor:
         self.filename = filename
         self.features_file = file_output
         
-    def envelope_amplitude(self, signal, sr, window_size):
-        # Define o passo de deslizamento da janela para fazer subamostragem de fator 2
-        hop_length = window_size
+    def envelope_amplitude(self, y, window_size):
+        analytic_signal = signal.hilbert(y)
+        amplitude_envelope = np.abs(analytic_signal)
         
-        # Divide o sinal em janelas sobrepostas com tamanho window_size e passo hop_length
-        signal_frames = librosa.util.frame(signal, frame_length=window_size, hop_length=hop_length)
-        
-        # Aplica a janela de Hann a cada janela
-        window = librosa.filters.hann(window_size)
-        signal_frames_windowed = signal_frames * window[:, np.newaxis]
-        
-        # Calcula o valor absoluto do sinal suavizado em cada janela
-        signal_abs = np.abs(signal_frames_windowed)
-        
-        # Calcula os picos do sinal em cada janela
-        peaks = np.argmax(signal_abs, axis=0)
-        
-        # Cria um vetor de tempo com a mesma dimensão do sinal
-        time = np.arange(0, len(signal))/sr
-        
-        # Interpola os pontos dos picos
-        interp_fn = interp1d(time[peaks], signal_abs[peaks, range(len(peaks))], kind='cubic', fill_value='extrapolate')
-        
-        # Retorna o envelope de amplitude interpolado
-        return interp_fn(time)
+        # Criando um array vazio para armazenar os valores subamostrados
+        sub = [0]
+
+        # Iterando sobre o sinal
+        for i in range(0, len(amplitude_envelope), window_size):
+            # Obtendo a janela atual
+            y_window = y[i:i+window_size]
+
+            # Calculando a média dos valores da janela e adicionando ao array de subamostragem
+            sub.append(np.mean(y_window))
+
+        return np.array(sub)
         
     def extract_features(self, n_fft):
         with h5py.File(self.filename, "r") as f:
@@ -59,40 +49,40 @@ class AudioFeatureExtractor:
             for i, (audio_array, annotation) in enumerate(zip(audio, annotations)):
                 
                 # AE - Amplitude Envelope
-                ae = self.envelope_amplitude(signal=audio_array,sr=16000, window_size=n_fft)
+                ae = self.envelope_amplitude(y=audio_array, window_size=n_fft).reshape((-1,1))
 
                 # RMS - Root-Mean-Square
-                rms = librosa.feature.rms(y=audio_array, frame_length=n_fft, hop_length=n_fft)[0]
+                rms = librosa.feature.rms(y=audio_array, frame_length=n_fft, hop_length=n_fft)[0].reshape((-1,1))
 
                 # ZCR - Zero-Crossing Rate
-                zcr = librosa.feature.zero_crossing_rate(y=audio_array, frame_length=n_fft, hop_length=n_fft)[0]
+                zcr = librosa.feature.zero_crossing_rate(y=audio_array, frame_length=n_fft, hop_length=n_fft)[0].reshape((-1,1))
 
                 # BER - Band Energy Ratio
-                ber = librosa.feature.spectral_bandwidth(y=audio_array, sr=16000, n_fft=n_fft, hop_length=n_fft)[0]
+                ber = librosa.feature.spectral_bandwidth(y=audio_array, sr=16000, n_fft=n_fft, hop_length=n_fft)[0].reshape((-1,1))
 
                 # SC - Spectral Centroid
-                sc = librosa.feature.spectral_centroid(y=audio_array, sr=16000, n_fft=n_fft, hop_length=n_fft)[0]
+                sc = librosa.feature.spectral_centroid(y=audio_array, sr=16000, n_fft=n_fft, hop_length=n_fft)[0].reshape((-1,1))
 
                 # BW - Frequency Bandwidth
-                bw = librosa.feature.spectral_bandwidth(y=audio_array, sr=16000, n_fft=n_fft, hop_length=n_fft)[0]
+                bw = librosa.feature.spectral_bandwidth(y=audio_array, sr=16000, n_fft=n_fft, hop_length=n_fft)[0].reshape((-1,1))
 
                 # SF - Spectral Flux
-                sf = librosa.onset.onset_strength_multi(y=audio_array, sr=16000, n_fft=n_fft, hop_length=n_fft)[0]
+                sf = librosa.onset.onset_strength_multi(y=audio_array, sr=16000, n_fft=n_fft, hop_length=n_fft)[0].reshape((-1,1))
 
                 # MFCC - Mel-frequency cepstral coefficients
                 mfcc = librosa.feature.mfcc(y=audio_array, sr=16000, n_mfcc=22, n_fft=n_fft, hop_length=n_fft)
                 
-                print('ae:', ae.shape)
-                print('rms:', rms.shape)
-                print('zcr:', zcr.shape)
-                print('ber:', ber.shape)
-                print('sc:', sc.shape)
-                print('bw:', bw.shape)
-                print('sf:', sf.shape)
-                print('mfcc:', mfcc.T.shape)
+                # print('ae:', ae.shape)
+                # print('rms:', rms.shape)
+                # print('zcr:', zcr.shape)
+                # print('ber:', ber.shape)
+                # print('sc:', sc.shape)
+                # print('bw:', bw.shape)
+                # print('sf:', sf.shape)
+                # print('mfcc:', mfcc.T.shape)
 
                 # concatena todas as features em um único array
-                feature_array = np.concatenate([ae, rms, zcr, ber, sc, bw, sf, mfcc.T])
+                feature_array = np.concatenate([ae, rms, zcr, ber, sc, bw, sf, mfcc.T], axis=1)
 
                 if i % 1000 == 0:
                     print(f"Processado {i} audios")
