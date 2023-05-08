cf1f3ec83a9b778a44229d3e5868b520ad479652
caleo-hub <caleomenesessantos@gmail.com>
2023-05-07 21:16:28 -0300

    vsd integration tentative 1

--- src/dataset_preprocessing/vsd_dataset_generator.py
+++ src/dataset_preprocessing/vsd_dataset_generator.py
@@ -63,73 +63,128 @@ class VSD_DatasetGenerator:
         return movie_annotations
             
 
-def plot_film_annotations(film_name, annotations):
-    # Cria dicionários separados para cada tipo de annotation
-    explosions = {'heights': [], 'color': 'red', 'label': 'Explosions'}
-    gunshots = {'heights': [], 'color': 'green', 'label': 'Gunshots'}
-    screams = {'heights': [], 'color': 'blue', 'label': 'Screams'}
-    scream_effort = {'heights': [], 'color': 'black', 'label': 'Scream Effort'}
-    multiple_actions = {'heights': [], 'color': 'orange', 'label': 'Multiple Actions'}
-    nothing = {'heights': [], 'color': 'grey', 'label': 'Nothing'}
-    others = {'heights': [], 'color': 'purple', 'label': 'Others'}
-    
-    annotation_list = annotations[film_name]
-    
-    # Cria um dicionário com as funções de tratamento de cada tipo de annotation
-    annotation_handlers = {
-        'explosion': lambda x, y, w: explosions['heights'].append((x, y, w)),
-        'gunshot': lambda x, y, w: gunshots['heights'].append((x, y + 1, w)),
-        'scream': lambda x, y, w: screams['heights'].append((x, y + 2, w)),
-        'scream_effort': lambda x, y, w: scream_effort['heights'].append((x, y + 3, w)),
-        'multiple_actions': lambda x, y, w: multiple_actions['heights'].append((x, y + 4, w)),
-        '(nothing)': lambda x, y, w: nothing['heights'].append((x, y + 5, w)),
-        'others': lambda x, y, w: others['heights'].append((x, y + 6, w))
-    }
-    
-    for start_time, end_time, annotation, _, duration in annotation_list:
-        # Calcula a largura e posição do retângulo
-        width = duration
-        x = start_time
-        y = 0  # posicao vertical inicial
-        # Adiciona o retângulo à lista correspondente ao tipo de annotation
-        annotation_handlers.get(annotation, annotation_handlers['others'])(x, y, width)
+    def plot_film_annotations(self, film_name, annotations=None):
+        if annotations is None:
+            annotations = self.get_movie_original_annotations()
         
-    # Cria uma figura e um eixo
-    fig, ax = plt.subplots(figsize=(20, 3))
-    
-    # Plota retângulos para cada tipo de annotation
-    heights = [explosions, gunshots, screams, scream_effort, multiple_actions, nothing, others]
-    
-    for i, h in enumerate(heights):
-        if len(h['heights']) == 0:
-            continue 
-        xs, ys, widths = zip(*h['heights'])
-        rects = ax.bar(xs, 1, widths, bottom=ys, color=h['color'], alpha=0.5, label=h['label'])
-        for rect in rects:
-            rect.set_linewidth(0)
-    
-    # Adiciona as legendas
-    ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
-    print()
-    ax.set_xlim(0, annotation_list[-1][1])
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Annotations')
-    ax.set_title(film_name)
-    
-    # Remove os ticks do eixo y
-    ax.tick_params(axis='y', which='both', length=0)
-    plt.title(film_name)
+        # Cria dicionários separados para cada tipo de annotation
+        colors = {
+            'explosion': 'red',
+            'gunshot': 'green',
+            'scream': 'blue',
+            'scream_effort': 'black',
+            'multiple_actions': 'orange',
+            '(nothing)': 'grey',
+            'others': 'purple',
+        }
+        
+        annotation_list = annotations[film_name]
+        
+        # Cria uma figura e um eixo
+        fig, ax = plt.subplots(figsize=(20, 3))
+        
+        for start_time, end_time, annotation, _, duration in annotation_list:
+            # Calcula a largura e posição do retângulo
+            width = round(duration, 2)
+            x = round(start_time, 2)
+            y = 0  # posicao vertical inicial
+            
+            # Adiciona o retângulo à lista correspondente ao tipo de annotation
+            rect_color = colors.get(annotation, colors['others'])
+            rect = plt.Rectangle((x, y), width, 1, color=rect_color, alpha=0.5)
+            ax.add_patch(rect)
+        
+        # Adiciona as legendas
+        custom_lines = [plt.Line2D([0], [0], color=color, lw=4) for color in colors.values()]
+        ax.legend(custom_lines, colors.keys(), loc='upper left', bbox_to_anchor=(1, 1))
+        
+        ax.set_xlim(0, annotation_list[-1][1])
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Annotations')
+        ax.set_title(film_name)
+        
+        # Remove os ticks do eixo y
+        ax.tick_params(axis='y', which='both', length=0)
+        plt.title(film_name)
 
-    plt.show()
+        plt.show()
 
+    def remove_nothing_annotations(self, movie_annotations):
+        no_nothing_annotations = {}
+        for movie, annotations in movie_annotations.items():
+            no_nothing_annotations[movie] = [annotation for annotation in annotations if annotation[2] != '(nothing)']
+        return no_nothing_annotations
+        
+    def remove_inner_annotations(self, annotations):
+        temp_annotations = []
+        for i, current_annotation in enumerate(annotations):
+            is_inner = False
+            for j, other_annotation in enumerate(annotations):
+                if i != j and current_annotation[0] >= other_annotation[0] and current_annotation[1] <= other_annotation[1]:
+                    is_inner = True
+                    break
+            if not is_inner:
+                temp_annotations.append(current_annotation)
+        return temp_annotations
     
+    def find_intersections(self, temp_annotations):
+        updated_annotations = []
+        added = set()
+
+        for i, current_annotation in enumerate(temp_annotations):
+            if i in added:
+                continue
+
+            has_intersection = False
+            for j, other_annotation in enumerate(temp_annotations):
+                if i != j and current_annotation[1] > other_annotation[0] and current_annotation[0] < other_annotation[1]:
+                    has_intersection = True
+                    start_time = min(current_annotation[0], other_annotation[0])
+                    end_time = max(current_annotation[1], other_annotation[1])
+                    duration = round(end_time - start_time, 3)
+                    updated_annotations.append((start_time, end_time, current_annotation[2], current_annotation[3], duration))
+                    added.add(j)
+                    break
+
+            if not has_intersection:
+                updated_annotations.append(current_annotation)
+                added.add(i)
+
+        return updated_annotations
+    
+    def add_nothing_annotations(self, annotations):
+        complete_annotations = annotations.copy()
+        timeline = sorted(annotations, key=lambda x: x[0])
+        
+        complete_annotations.append((0.0, timeline[0][0], '(nothing)', '', timeline[0][0]))
+        for i in range(len(timeline) - 1):
+            current_annotation = timeline[i]
+            next_annotation = timeline[i + 1]
+            
+            if current_annotation[1] < next_annotation[0]:
+                start_time = current_annotation[1]
+                end_time = next_annotation[0]
+                duration = end_time - start_time
+                complete_annotations.append((start_time, end_time, '(nothing)', '', duration))
+        
+        return sorted(complete_annotations, key=lambda x: x[0])
+    
+    def optimize_annotations(self):
+        original_annotations = self.get_movie_original_annotations()
+        no_nothing_annotations = self.remove_nothing_annotations(original_annotations)
+        
+        optimized_annotations = {}
+        for movie, annotations in no_nothing_annotations.items():
+            temp_annotations = self.remove_inner_annotations(annotations)
+            updated_annotations = self.find_intersections(temp_annotations)
+            complete_annotations = self.add_nothing_annotations(updated_annotations)
+            optimized_annotations[movie] = complete_annotations
+        
+        return optimized_annotations
+            
+
 if __name__ == '__main__':
     vsd_gen = VSD_DatasetGenerator(path='C:/Users/CSANT321/Documents/TCC/violence-detection-acoustic-scenes/Datasets/VSD_2014_December_official_release/Hollywood-dev/annotations')
     movie_annotations = vsd_gen.get_movie_original_annotations()
-    
-    import json
-    
-    with open("src/dataset_analysis/vsd_annotations.json", "w") as f:
-        json.dump(movie_annotations, f)
-        
-    plot_film_annotations('Armageddon', movie_annotations)
+    new_annotations = vsd_gen.optimize_annotations()
+    vsd_gen.plot_film_annotations('Armageddon', new_annotations)
---
+++ src/dataset_preprocessing/vsd_feature_extractor.py
@@ -0,0 +1,81 @@
+import os
+import numpy as np
+import h5py
+import librosa
+
+class FilmFeatureExtractor:
+    def __init__(self, annotations, mat_files_path):
+        self.annotations = annotations
+        self.mat_files_path = mat_files_path
+
+    def resample_feature(self, feature, original_sr, target_sr):
+        resample = librosa.resample(y=feature.T, orig_sr=original_sr, target_sr=target_sr)
+        return resample.T
+    
+    def process_mat_file(self, mat_file_path, original_sr=25, target_sr=16000):
+        with h5py.File(mat_file_path, 'r') as mat_data:
+            ae = self.resample_feature(np.array(mat_data['AE']), original_sr, target_sr)
+            rms = self.resample_feature(np.array(mat_data['RMS']), original_sr, target_sr)
+            zcr = self.resample_feature(np.array(mat_data['ZCR']), original_sr, target_sr)
+            ber = self.resample_feature(np.array(mat_data['BER']), original_sr, target_sr)
+            sc = self.resample_feature(np.array(mat_data['SC']), original_sr, target_sr)
+            bw = self.resample_feature(np.array(mat_data['BW']), original_sr, target_sr)
+            sf = self.resample_feature(np.array(mat_data['SF']), original_sr, target_sr)
+            mfcc = self.resample_feature(np.array(mat_data['MFCC']), original_sr, target_sr)
+
+            feature_array = np.concatenate([ae, rms, zcr, ber, sc, bw, sf, mfcc], axis=1)
+            
+            return feature_array
+
+    def process_all_films(self, output_file_path):
+        with h5py.File(output_file_path, 'w') as hf:
+            for film_name in self.annotations:
+                print('Processando:', film_name)
+                mat_file_path = os.path.join(self.mat_files_path, f"{film_name}_auditory.mat")
+                if os.path.exists(mat_file_path):
+                    feature_array = self.process_mat_file(mat_file_path)
+                    hf.create_dataset(film_name, data=feature_array)
+                else:
+                    print(f"Arquivo .mat não encontrado para o filme {film_name}.")
+    
+    def create_labeled_features(self, features_h5_path, movie_annotations, output_h5_path):
+        annotation_dict = {
+            '(nothing)': 0,
+            'scream': 1,
+            'Violência Física': 2,
+            'gunshot': 3,
+            'explosion': 4,
+            'multiple_actions': 5
+        }
+
+        target_length = 10 * 16000
+
+        with h5py.File(features_h5_path, 'r') as features_h5, h5py.File(output_h5_path, 'w') as output_h5:
+            annotations_group = output_h5.create_group('annotations')
+            features_group = output_h5.create_group('features')
+
+            for movie, movie_features in features_h5.items():
+                for idx, (start_time, end_time, annotation, _, _) in enumerate(movie_annotations[movie]):
+                    annotation = annotation_dict.get(annotation, annotation_dict['(nothing)'])
+                    start_sample = np.floor(start_time * 16000).astype(int)
+                    end_sample = np.ceil(end_time * 16000).astype(int)
+                    total_samples = end_sample - start_sample
+
+                    if annotation == annotation_dict['(nothing)'] and total_samples <= target_length:
+                        padded_features = librosa.util.pad_center(movie_features[start_sample:end_sample, :].astype(np.float32), size=target_length, axis=0)
+                        dataset_name = f"{movie}_{idx}"
+                        features_group.create_dataset(dataset_name, data=padded_features)
+                        annotations_group.create_dataset(dataset_name, data=np.array([annotation], dtype=np.int8))
+                        print(dataset_name)
+                        
+                    elif annotation != annotation_dict['(nothing)']:
+                        num_slices = int(np.ceil(total_samples / target_length))
+                        for i in range(num_slices):
+                            slice_start = start_sample + i * target_length
+                            slice_end = min(slice_start + target_length, end_sample)
+                            sliced_features = movie_features[slice_start:slice_end, :].astype(np.float32)
+                            padded_features = librosa.util.pad_center(sliced_features, size=target_length, axis=0)
+                            dataset_name = f"{movie}_{idx}_{i}"
+                            features_group.create_dataset(dataset_name, data=padded_features)
+                            annotations_group.create_dataset(dataset_name, data=np.array([annotation], dtype=np.int8)) 
+                            print(dataset_name)
